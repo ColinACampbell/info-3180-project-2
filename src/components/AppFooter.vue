017ee14a2f26bc11c01f161388adf41e05683c09
Colin Campbell <prontobol@gmail.com>
2022-04-13 11:18:04 -0500

    Initial commit

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,21 @@
+<template>
+      <footer class="mt-5">
+          <div class="container">
+              <p>Copyright &copy {{ year }} Flask Inc.</p>
+          </div>
+      </footer>
+</template>
+
+<script>
+export default {
+    data() {
+      return {
+          year: (new Date).getFullYear()
+      }        
+    }
+}
+</script>
+
+<style>
+/* Add any component specific styles here */
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    data() {
+        return {
+            message: "Hello World! This is Lab 7"
+        };
+    },
+}
+</script>
+
+<template>
+    <div class="container">
+      <div class="text-center">
+        <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+        <h1>{{ message }}</h1>
+      </div>
+    </div>
+</template>
+
+<style>
+/* Add any component specific styles here */
+</style>
