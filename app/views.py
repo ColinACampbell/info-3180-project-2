5b8c0914ed57fc2adcb5f3771c8ebe98e8f257ca
Deezum <beadleselena@gmail.com>
2022-04-15 22:34:16 -0500

    endpoints

--- app/views.py
+++ app/views.py
@@ -5,10 +5,16 @@ Werkzeug Documentation:  https://werkzeug.palletsprojects.com/
 This file creates your application.
 """
 
-from app import app
-from flask import render_template, request, jsonify, send_file
+from app import app,db, login_manager
+from flask import render_template, request, jsonify,url_for, send_file, redirect,flash
 import os
-
+from app import app 
+from flask_login import login_user, logout_user, current_user, login_required
+from app.forms import LoginForm
+from app.models import UserProfile
+from werkzeug.security import check_password_hash
+from werkzeug.utils import secure_filename
+from flask_wtf.csrf import generate_csrf
 
 ###
 # Routing for your application.
@@ -16,12 +22,133 @@ import os
 
 @app.route('/')
 def index():
-    return jsonify(message="This is the beginning of our API")
+    return send_file(os.path.join('../dist/', 'index.html'))
 
 
 ###
 # The functions below should be applicable to all Flask apps.
 ###
+@app.route("/api/auth/logout", methods=["POST"])
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.', 'danger')
+    return redirect(url_for("index"))
+
+@app.route("/api/auth/login", methods=[ "POST"])
+def login():
+    form = LoginForm()
+    if request.method == "POST" and form.validate_on_submit():
+        if form.username.data:
+            username = form.username.data
+            password = form.password.data
+
+            user = UserProfile.query.filter_by(username=username).first()
+
+            if user is not None and check_password_hash(user.password, password):
+                login_user(user)
+                flash("Successful login! ", "success")
+                return redirect(url_for("")) 
+            else:
+                flash("Login denied. Try again!", "danger")
+
+            return redirect(url_for("index"))
+    return render_template("login.html", form=form)
+
+@app.route('/api/register', methods=['POST'])
+def register():
+    createuser = UserForm()
+   
+    if request.method == 'POST':
+        createuser.username.data = request.form['username']
+        createuser.password.data = request.form['password']
+        createuser.firstname.data = request.form['firstname']
+        createuser.lastname.data = request.form['lastname']
+        createuser.emailaddress.data = request.form['emailaddress']
+        createuser.location.data = request.form['location']
+        createuser.bio.data = request.form['bio']
+        createuser.photo.data = request.files['photo']
+        
+        if createuser.validate_on_submit():
+            username = createuser.username.data
+            password = createuser.password.data
+            firstname = createuser.firstname.data
+            lastname = createuser.lastname.data
+            emailaddress = createuser.emailaddress.data
+            location = createuser.location.data
+            bio = createuser.bio.data
+            photo = createuser.profile_photo.data
+
+            filename = secure_filename(photo.filename)
+            dbuser = UserProfile(username, password, firstname, lastname, emailaddress, location, bio, filename)
+            db.session.add(dbuser)
+            db.session.commit()
+            profile_photo.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            Formdata = {
+            "message": "File upload was successful!",
+            "filename": filename,
+            }
+            return jsonify(formdata=formdata)
+        else:
+            errordata = {
+            "errors": [{"filename":form_errors(createuser)}]
+        }
+        return jsonify(errordata=errordata)
+
+
+            
+    
+@app.route('/api/cars', methods=['GET'])
+def returncars():
+    return car
+@app.route('/api/cars', methods=['POST'])
+def addcars():
+    addcar = AddCarForm()
+   
+    if request.method == 'POST':
+        if addcar.validate_on_submit():
+
+            # Collect the data from the form
+            make = request.addcar['make']
+            description = request.addcar['description']
+            model = request.addcar['model']
+            colour = request.addcar['colour']
+            year = request.addcar['year']
+            transmission = request.addcar['transmission']
+            car_type = request.addcar['car_type']
+            price = request.addcar['price']
+            file = request.files['photo']
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            mycar = Car(make, description, model, colour, year, transmission, car_type,price, filename)
+            db.session.add(mycar)
+            db.session.commit()
+
+            flash('New car has been successfully added!', 'success')
+            return redirect(url_for(''))
+        else:
+            flash("Error!")
+            return render_template('', form=addcar)
+    elif request.method == 'GET':
+        return render_template('', form=addcar) 
+
+@app.route('/api/cars/{car_id}', methods=['GET'])
+def cardetail():
+    return car
+@app.route('/api/cars/{car_id}/favourites', methods=['POST'])
+def favcar():
+    return car
+@app.route('/api/searh', methods=['GET'])
+def searchcars():
+    return car
+@app.route('/api/users/{user_id}', methods=['GET'])
+def userdetails():
+    return car
+
+@app.route('/api/users/{user_id}/favourites', methods=['GET'])
+def userfavs():
+    return car
 
 # Here we define a function to collect form errors from Flask-WTF
 # which we can later use
