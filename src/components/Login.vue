31ece9d0afeef10a3e6cccff24c02afb7fa82ffa
Jevon Forrest <jevonforrest77@gmail.com>
2022-04-17 19:56:00 -0500

    login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,6 @@
 <template>
 
- <form @submit.prevent="uploadPhoto" id="loginForm" method="POST" enctype="multipart/form-data">'
+ <form @submit.prevent="default" id="loginForm" method="POST" enctype="multipart/form-data">'
 
 	<label class="label" for="username">Username</label>
     <input type="text" name="name" id="name" class="form-control" placeholder="Enter your Username"/>
@@ -14,6 +14,47 @@
 
 <script>
 
+export default {
+        data() {
+            return {
+                csrf_token: '',
+            }
+        },
+        methods: {
+            methods: {
+            login() {
+
+                const self = this;
+                let loginForm = document.getElementById('loginForm');
+                let form_data = new FormData(loginForm);
+
+                fetch("/api/login", {method: 'POST', body: form_data,
+                headers: {
+                    'X-CSRFToken': this.csrf_token
+                    }
+                }).then(function (response) {
+                    return response.json();
+                }).then(function (data) {
+                    // display a success message
+                    console.log(data);
+                }).catch(function (error) {
+                    console.log(error);
+                });
+            },
+            getCsrfToken() {
+                let self = this;
+                fetch('/api/csrf-token')
+                .then((response) => response.json())
+                .then((data) => {
+                console.log(data);
+                self.csrf_token = data.csrf_token;
+            })
+            }
+        },
+        created() {
+            this.getCsrfToken();
+        }
+    }
 </script>
 
 <style>
