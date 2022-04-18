37c86440f917a7acbf71afd9f01709c022d86f1e
Jevon Forrest <jevonforrest77@gmail.com>
2022-04-17 19:59:13 -0500

    login view

---
+++ src/views/LoginView.vue
@@ -0,0 +1,24 @@
+<script>
+import loginForm from './../components/LoginForm.vue'
+
+    export default {
+        data() {
+            return {};
+        },
+        components: { LoginForm }
+    };
+</script>
+
+<template>
+    <div class="form-container">
+        <h2>LoginForm</h2>
+        <LoginForm />
+    </div>
+</template>
+
+<style>
+h2 {
+    display: flex;
+    justify-content: center;
+}
+</style>
